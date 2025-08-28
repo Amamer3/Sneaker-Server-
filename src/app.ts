fc09941f18385d0f5b4832440b6fe64fd4a2bd38
Amamer3 <tisyastephen@gmail.com>
2025-08-28 15:55:27 +0000

    Fix CORS headers - Add x-frame-options to allowedHeaders

--- src/app.ts
+++ src/app.ts
@@ -59,7 +59,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'x-content-type-options', 'x-csrf-token', 'x-xss-protection']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'x-content-type-options', 'x-csrf-token', 'x-xss-protection', 'x-frame-options']
 }));
 app.use(helmet({
   contentSecurityPolicy: false 
